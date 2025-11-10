111df0b70e14c8d62aa2910a7d192767b5931c07
panji090806 <dapan275@gmail.com>
2025-11-10 12:38:49 +0700

    pertemuan4

--- LuasLingkaran-pbo.php
+++ LuasLingkaran-pbo.php
@@ -1,16 +1,80 @@
 <?php
 
-class LuasLingkaran {
-    
-    public const PHI = 3.14;
-    public int $jariJari;
+namespace App\math;
+
+class Luaslingkaran 
+{
+    public const phi = 3.14;
+    public int $jari;
+
+    public function __construct($isijari = 1)
+
+    {
+        $this->jari = $isijari;
+    }
+
+    public function tampilLl($nama = 'ban')  
+    {
+        $rumus = Luaslingkaran ::phi * ($this->jari * $this->jari);
+        echo "Lingkaran" . ($nama) . " ini hasil luarnya adalah : " . $rumus . "<br/>";
+    }
+
+    public static function testing()
+    {
+        echo "</br>";
+        echo "Ini testing static<br>";
+    }
+    public function __destruct()
+    {
+        echo "udah capek luas<br>";
+    }
+    // {
+    //     echo "ini testing static";
+    // }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jariJari = 10;
 
-#echo LuasLingkaran::PHP#
 
-$rumus = LuasLingkaran::PHI * $lingkaran->jariJari * $lingkaran->jariJari;
-echo "hasilnya adalah $rumus";
+class Kelilinglingkaran
+{
+    
+    public const phi = 3.14;
+    public int $jari;
+    public function __construct($isijari = 1)
+
+    {
+        $this->jari = $isijari;
+    }
+
+    public function tampilKl($nama = 'ban')
+    {
+        $rumus = 2 * Kelilinglingkaran ::phi * $this->jari;
+        echo "Lingkaran" . ($nama) . " hasil Kelilingnya adalah : " . $rumus2 . "<br>";
+    }
+
+    public static function testing()
+    {
+        echo "</br>";
+        echo "ini testing static keliling <br>";
+    }
+
+    public function __destruct()
+    {
+        echo "udah capek keliling<br>";
+    }
+    // __construct()
+    // echo "ini dari constructor";
+    // echo "<br/>";
+}
+
+$lingkaran = new Luaslingkaran();
+$lingkaran->jari = 10;
+$lingkaran->tampilLl("ban");
+
+$lingkaran2 = new Kelilinglingkaran();
+$lingkaran2->jari = 10;
+$lingkaran2->tampilKl("ban");
+
+Luaslingkaran::testing();
+Kelilinglingkaran::testing();
 ?>
