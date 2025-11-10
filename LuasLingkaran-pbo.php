6425e197f4081ffe43a0d3ba2fa961b42ba500ee
panji090806 <dapan275@gmail.com>
2025-11-10 12:44:08 +0700

    pertemuan5

--- LuasLingkaran-pbo.php
+++ LuasLingkaran-pbo.php
@@ -1,80 +1,28 @@
-<?php
-
-namespace App\math;
-
-class Luaslingkaran 
-{
-    public const phi = 3.14;
-    public int $jari;
-
-    public function __construct($isijari = 1)
-
-    {
-        $this->jari = $isijari;
-    }
-
-    public function tampilLl($nama = 'ban')  
-    {
-        $rumus = Luaslingkaran ::phi * ($this->jari * $this->jari);
-        echo "Lingkaran" . ($nama) . " ini hasil luarnya adalah : " . $rumus . "<br/>";
-    }
-
-    public static function testing()
-    {
-        echo "</br>";
-        echo "Ini testing static<br>";
-    }
-    public function __destruct()
-    {
-        echo "udah capek luas<br>";
-    }
-    // {
-    //     echo "ini testing static";
-    // }
-}
-
-
-
-class Kelilinglingkaran
-{
+<?php 
+    class LuasLingkaran {
+        public const phi = 3.14;
+        public int $jari;
     
-    public const phi = 3.14;
-    public int $jari;
-    public function __construct($isijari = 1)
+        public function __construct($isijari = 1) {
+            $this->jari = $isijari;
+        }
+        public function tampil($nama = "ban") {
+            $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+            echo "Lingkaran {$nama} hasilnya adalah {$rumus}";
+        }    
 
-    {
-        $this->jari = $isijari;
+        public static function testing() {
+        echo '</br>';
+        echo 'ini testing static';
     }
 
-    public function tampilKl($nama = 'ban')
-    {
-        $rumus = 2 * Kelilinglingkaran ::phi * $this->jari;
-        echo "Lingkaran" . ($nama) . " hasil Kelilingnya adalah : " . $rumus2 . "<br>";
+    public function __destruct() {
+        echo '</br>';
+        echo 'selesai';
     }
-
-    public static function testing()
-    {
-        echo "</br>";
-        echo "ini testing static keliling <br>";
-    }
-
-    public function __destruct()
-    {
-        echo "udah capek keliling<br>";
-    }
-    // __construct()
-    // echo "ini dari constructor";
-    // echo "<br/>";
 }
-
-$lingkaran = new Luaslingkaran();
-$lingkaran->jari = 10;
-$lingkaran->tampilLl("ban");
-
-$lingkaran2 = new Kelilinglingkaran();
-$lingkaran2->jari = 10;
-$lingkaran2->tampilKl("ban");
-
-Luaslingkaran::testing();
-Kelilinglingkaran::testing();
+    $lingkaran = new luaslingkaran(10); 
+    // $lingkaran->jari = 10;
+    $lingkaran->tampil('roda'); 
+    LuasLingkaran::testing(); 
 ?>
